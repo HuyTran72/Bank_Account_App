6427b23fbeb2d866d26218d1ce9ba40687a16ffd
Huy Tran <baohuy82.th@gmail.com>
2023-09-07 22:35:53 -0700

    update

--- Bank.java
+++ Bank.java
@@ -1,20 +1,13 @@
 public class Bank {
     public static void main(String[] args){
+       
+        AccountManager accountManager = new AccountManager(new BankAccount[]{new BankAccount(1, "Kenny", 1000), new BankAccount(2, "Jimmy", 2000), new BankAccount(3, "Henry", 3000), new BankAccount(4, "Nicky", 4000), new BankAccount(5, "Terry", 5000)});
         
-        //create a new AccountManager object
-        AccountManager accountManager = new AccountManager(new BankAccount[]{new BankAccount(1, "Kenny", 1000), new BankAccount(2, "Jimmy", 2000), new BankAccount(3, "Henry", 3000)});
-        
-        //create two new accounts
-        accountManager.create(new BankAccount(4, "Alex", 4000));
-        accountManager.create(new BankAccount(5, "Rita", 5000));
-        
-        //transfer money from account 0 to the other accounts
-        accountManager.accounts[0].transfer(accountManager.accounts[1], 500);
-        accountManager.accounts[0].transfer(accountManager.accounts[2], 500);
-        accountManager.accounts[0].transfer(accountManager.accounts[3], 500);
+        accountManager.accounts[1].transfer(accountManager.accounts[4], 500);
+        accountManager.accounts[2].transfer(accountManager.accounts[4], 500);
+        accountManager.accounts[3].transfer(accountManager.accounts[4], 500);
         accountManager.accounts[0].transfer(accountManager.accounts[4], 500);
         
-        //print the balance of each account
         for(int i=0; i<accountManager.accounts.length; i++){
             System.out.println(accountManager.accounts[i].getBalance());
         }
